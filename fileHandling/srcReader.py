4a5caeb5d9e7250c73756fa56e0ec47ad10ab72f
shanchathusanda93 <shanchathusanda@gmail.com>
2018-11-07 18:42:26 -0100

    modify srcReader for ubuntu

--- fileHandling/srcReader.py
+++ fileHandling/srcReader.py
@@ -1,11 +1,18 @@
 import re
 
-with open("C:\\xampp\\htdocs\\research\\frontend\\front.php", "r") as file:
+# for windows
+# with open("C:\\xampp\\htdocs\\research\\frontend\\front.php", "r") as file:
+#     orig = file.read()
+#     text = orig.replace('\n', '')
+#     stringLength = len(text)
+
+# for ubuntu
+# did not work well like windows. Please reconsider
+with open("/home/shan/php_codes/simple_php_project/frontend/front.php", "r") as file:
     orig = file.read()
     text = orig.replace('\n', '')
     stringLength = len(text)
 
-# develop a for loop for this section for the try block
 try:
     found = re.search('<?php(.+?)?>', text).group(1)
     counter = text.index("?>")
